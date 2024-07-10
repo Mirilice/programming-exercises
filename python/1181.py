9939c2299933617ef915bb0181fef6600316e335
Mirilice <mariaalicepco10@gmail.com>
2024-07-10 18:18:21 -0300

    New exercise 1181

---
+++ python/1181.py
@@ -0,0 +1,16 @@
+linha = int(input())
+operacao = input()
+
+soma, media, cont = 0
+
+for i in range(12):
+    for j in range(12):
+        num = float(input())
+        if i == linha:
+            soma += num
+            cont += 1
+
+if operacao == 'S':
+    print(f'{soma:.1f}')
+else: print(f'{soma/cont:.1f}')
+
